dd6bffbbcab99d160ef8ac80efeebd20d0ec971c
motinMTN <luigi.motin@gmail.com>
2025-05-15 11:00:15 -0600

    no force https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -29,7 +29,7 @@ class AppServiceProvider extends ServiceProvider
         Schema::defaultStringLength(191);
 
         if (config('app.env') !== 'local') {
-            URL::forceScheme('https');
+            // URL::forceScheme('https');
         }
     }
 }
