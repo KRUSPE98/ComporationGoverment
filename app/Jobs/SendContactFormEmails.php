6b980a44378e146e1576fa1833e70c5904379d23
motinMTN <luigi.motin@gmail.com>
2025-05-14 18:48:28 -0600

    try catch al bloque del handle del job

--- app/Jobs/SendContactFormEmails.php
+++ app/Jobs/SendContactFormEmails.php
@@ -9,6 +9,7 @@ use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Facades\Log;
 
 class SendContactFormEmails implements ShouldQueue
 {
@@ -23,24 +24,32 @@ class SendContactFormEmails implements ShouldQueue
 
     public function handle()
     {
-        // Enviar al admin
-        Mail::to($this->data['adminEmails'])->send(new ContactFormMail(
-            $this->data['name'],
-            $this->data['email'],
-            $this->data['phone'],
-            $this->data['message'],
-            'emails.admin_contact',
-            'Nuevo mensaje de contacto'
-        ));
+        try {
+            // Enviar al admin
+            Mail::to($this->data['adminEmails'])->send(new ContactFormMail(
+                $this->data['name'],
+                $this->data['email'],
+                $this->data['phone'],
+                $this->data['message'],
+                'emails.admin_contact',
+                'Nuevo mensaje de contacto'
+            ));
 
-        // Enviar al usuario
-        Mail::to($this->data['email'])->send(new ContactFormMail(
-            $this->data['name'],
-            $this->data['email'],
-            null,
-            null,
-            'emails.user_confirmation',
-            'Confirmación de mensaje enviado'
-        ));
+            // Enviar al usuario
+            Mail::to($this->data['email'])->send(new ContactFormMail(
+                $this->data['name'],
+                $this->data['email'],
+                null,
+                null,
+                'emails.user_confirmation',
+                'Confirmación de mensaje enviado'
+            ));
+        } catch (\Throwable $e) {
+            Log::error('Error al enviar correos desde el job SendContactFormEmails: ' . $e->getMessage(), [
+                'exception' => $e,
+                'data' => $this->data
+            ]);
+        }
     }
 }
+
